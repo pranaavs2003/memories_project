29aede8382d2a8ceb14a2028c50e47e1122c6a52
pranaavs2003 <pranaavs2003@gmail.com>
2022-05-31 17:26:26 +0530

    First

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,16 @@
+import React from "react";
+import useStyles from "./styles";
+
+function Post() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <h3>Post</h3>
+      <h3>Post</h3>
+      <h3>Post</h3>
+    </div>
+  );
+}
+
+export default Post;
